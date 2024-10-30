4579d23db26a768385de86fdca0083baf6c0a5bc
Henrique Borges <henriqueborgeshbr@gmail.com>
2024-10-30 09:30:55 -0300

    Reset file upload input after submission

--- components/upload-contract-button.tsx
+++ components/upload-contract-button.tsx
@@ -68,6 +68,10 @@ export const UploadContractButton = () => {
       }
     } finally {
       setUploading(false);
+
+      if (hiddenFileInput.current) {
+        hiddenFileInput.current.value = "";
+      }
     }
   };
 
