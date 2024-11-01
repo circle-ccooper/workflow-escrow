aedf5a837e9209b9a5a996c9bda1b1a414aaf882
Frederico Dal Grande <dalgrande@gmail.com>
2024-11-01 17:19:53 -0300

    add index

--- supabase/migrations/00005_add_wallet_address_and_more.sql
+++ supabase/migrations/00005_add_wallet_address_and_more.sql
@@ -5,6 +5,8 @@ ALTER TABLE wallets
     ADD COLUMN account_type VARCHAR(50),
     ADD COLUMN blockchain VARCHAR(50);
 
+CREATE INDEX idx_wallets_address ON wallets(wallet_address);
+
 -- Add comments for clarity
 COMMENT ON COLUMN wallets.wallet_set_id IS 'Reference to the wallet set';
 COMMENT ON COLUMN wallets.wallet_address IS 'Blockchain wallet address';
